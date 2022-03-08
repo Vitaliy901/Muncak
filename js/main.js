2657a388aa4a08e244da1c05135595dc0e33c94d
Vitaliy901 <allanamos90@gmail.com>
2022-03-08 10:14:49 +0200

    Computition support in js done.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 let toTop = document.querySelector('#toTop')
 let footer = document.querySelector('footer')
 let header = document.querySelector('header')
+let nums = document.querySelectorAll('.count')
+let support = document.querySelector('.support-wrapper')
 // =======scroll toTop
 window.addEventListener('scroll', function (e) {
 	let size = footer.offsetTop - window.innerHeight
@@ -25,3 +27,42 @@ window.addEventListener('mousemove', function (e) {
 })
 
 // ========count
+let arr = []
+let quantityStart = 0;
+let startFirst = 0;
+let startSecond = 111;
+let startThird = 10;
+
+window.addEventListener('scroll', function count(e) {
+	let size = support.offsetTop + header.offsetHeight  - this.innerHeight;
+	console.log(size)
+	console.log(this.scrollY + "!")
+	if (this.scrollY > size) {
+		for (let i = 0; i < nums.length; i++) {
+			arr.push(nums[i].innerHTML)
+			counts(nums[i], arr[i])
+		}
+		this.removeEventListener('scroll', count);
+	} 
+	
+})
+
+function counts(a, b) {
+	console.log("!")
+	let	id = setInterval(function (element, finish) {
+
+		if (element.innerHTML.length > 6) {
+			element.innerHTML = startSecond++ + ',' + startSecond++;
+		} 
+		if (element.innerHTML.length > 3 && element.innerHTML.length <= 6 ) {
+			element.innerHTML = startThird++ + ',' + startSecond++;
+		}
+		if (element.innerHTML.length  <= 3) {
+			element.innerHTML = startFirst++;
+		}
+		if (quantityStart++ > 108) {
+			element.innerHTML = finish
+			clearInterval(id)
+		}
+	}, 50, a,b)
+}
