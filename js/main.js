f3412738337d5f18bbcb8c2d7e471b9b24d79a60
Vitaliy901 <allanamos90@gmail.com>
2022-03-09 16:44:42 +0200

    fix bg in header.

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ window.addEventListener('mousemove', function (e) {
 	if (header.offsetWidth > 1400) {
 		header.style.backgroundPosition = `bottom ${e.clientY / speed / 100 + 15}% right ${e.clientX / speed / 100 + 5}%, 
 		left ${e.clientX / speed / 100 + 44}% top ${e.clientY / speed / 100 + 52}%, right  top, 
-		left -13% bottom, left ${e.clientX / speed / 100 + 1}% top ${e.clientY / speed / 100 + 40}%`;
+		left -15% bottom, left ${e.clientX / speed / 100 + 1}% top ${e.clientY / speed / 100 + 40}%`;
 	} else {
 		header.style.backgroundPosition = `bottom ${e.clientY / speed / 100 + 15}% right ${e.clientX / speed / 100 + 5}%, 
 		left ${e.clientX / speed / 100 + 44}% top ${e.clientY / speed / 100 + 52}%, right  top,left ${e.clientX / speed / 100 + 1}% top ${e.clientY / speed / 100 + 40}%`;
